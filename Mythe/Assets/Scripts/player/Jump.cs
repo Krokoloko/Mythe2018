3958d09b6b6c15bdd5b09e7579bdfd819b9a7970
Daan Ruting <daan.j.ruting@gmail.com>
2018-03-12 09:52:14 +0100

    Working on Player
    added a jump scipt(To be fixed)

---
+++ Mythe/Assets/Scripts/player/Jump.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour {
+
+    public enum jumpState {none, falling, rising };
+    private jumpState state = jumpState.none;
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void jump()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            //print("pressing");
+            PlayerPhysics_.velocity = new Vector2(PlayerPhysics_.velocity.x, ySpeed * _Speed_);
+            state = jumpState.rising;
+
+        }
+        if (Input.GetKeyUp(KeyCode.Space) || (originalPos + maxHeight) <= transform.position.x)
+        {
+            //print("released");
+            state = jumpState.falling;
+        }
+
+    }
+}
