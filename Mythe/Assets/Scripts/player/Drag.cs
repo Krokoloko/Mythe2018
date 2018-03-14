0653a0a5fbc93d3ab107624aa4bd2663b428a576
VolkenK <volkenkoelewijn@gmail.com>
2018-03-14 13:53:52 +0100

    player push
    voltooid

--- Mythe/Assets/Scripts/player/Drag.cs
+++ Mythe/Assets/Scripts/player/Drag.cs
@@ -6,25 +6,27 @@ public class Drag : MonoBehaviour
 {
 
     //  var hit : RaycastHit
-    private float pushforce = 1;
-    private float pullforce = 1;
+    public float pushforce;
+    public float pullforce = 1;
     private GameObject player;
     private Walk _playerWalk;
     private Rigidbody _rb;
+    private float _normSpeed; 
+
 
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player");
-        _playerWalk = player.GetComponent<Walk>();
+        _playerWalk = GetComponent<Walk>();
         _rb = GetComponent<Rigidbody>();
-
+        _normSpeed = _playerWalk.moveSpeed;
     }
 
 void Update()
     {
 
 
-        
+        Debug.Log(_playerWalk.moveSpeed);
+
 
     }
 
@@ -36,14 +38,26 @@ void Update()
 
             if (Input.GetKeyDown(KeyCode.L))
             {
-                _rb.constraints.
+                other.rigidbody.constraints = RigidbodyConstraints.FreezeRotation;
+                _playerWalk.moveSpeed *= (pushforce /10);
+
                 /*float distanceWanted = 3.0f;
 
                 Vector3 diff = transform.position - player.transform.position;
                 diff.y = 0.0f; // ignore Y (as requested in question)
                 transform.position = player.transform.position + diff.normalized * distanceWanted;*/
             }
+            if (Input.GetKeyUp(KeyCode.L))
+            {
+                _playerWalk.moveSpeed =_normSpeed;
+                other.rigidbody.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezeRotation;
+            }
         }
+    }
+    void OnCollisionExit(Collision other)
+    {
+        _playerWalk.moveSpeed = _normSpeed;
+        other.rigidbody.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezeRotation;
     }
 }
 
