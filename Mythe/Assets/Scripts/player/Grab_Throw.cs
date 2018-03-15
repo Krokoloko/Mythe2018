7ac4f07e438321e98aa70dda020dc6b2de513cf3
nickvaningen <nickvaningen@hotmail.com>
2018-03-15 13:35:29 +0100

    test

--- Mythe/Assets/Scripts/player/Grab_Throw.cs
+++ Mythe/Assets/Scripts/player/Grab_Throw.cs
@@ -6,25 +6,26 @@ public class Grab_Throw : MonoBehaviour
 
     public bool grabbed;
     RaycastHit hit;
-    public int distance = 2;
+    public float distance = 2f;
     public Transform holdpoint;
     public float throwforce;
     public LayerMask notgrabbed;
-
     void Update()
     {
+
         if (Input.GetKeyDown(KeyCode.LeftShift))
         {
+        
             if (!grabbed)
             {
-                Physics queriesStartInColliders = false;
+                /*Physics.queriesStartInColliders = false;*/
 
-                hit = Physics.Raycast(transform.position, Vector3.right * transform.localScale.x, distance);
+                hit = Physics.Raycast(transform.position, Vector2.right * transform.localScale.x, distance);
 
                 if (hit.collider != null && hit.collider.tag == "grabbable")
                 {
                     grabbed = true;
-
+                    Debug.Log("werkt");
                 }
             }
             else if (!Physics2D.OverlapPoint(holdpoint.position, notgrabbed))
@@ -34,13 +35,13 @@ public class Grab_Throw : MonoBehaviour
                 if (hit.collider.gameObject.GetComponent<Rigidbody>() != null)
                 {
 
-                    hit.collider.gameObject.GetComponent<Rigidbody>().velocity = new Vector3(transform.localScale.x, 1) * throwforce;
+                    hit.collider.gameObject.GetComponent<Rigidbody>().velocity = new Vector3(transform.localScale.x, 1,0) * throwforce;
                 }
             }
         }
-
         if (grabbed)
             hit.collider.gameObject.transform.position = holdpoint.position;
+        
     }
 
     void OnDrawGizmos()
@@ -49,4 +50,57 @@ public class Grab_Throw : MonoBehaviour
 
         Gizmos.DrawLine(transform.position, transform.position + Vector3.right * transform.localScale.x * distance);
     }
+    /*using UnityEngine;
+using System.Collections;
+
+public class Grab_Throw : MonoBehaviour
+{
+
+    public bool grabbed;
+    RaycastHit2D hit;
+    public float distance = 2f;
+    public Transform holdpoint;
+    public float throwforce;
+    public LayerMask notgrabbed;
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+        
+            if (!grabbed)
+            {
+                Physics2D.queriesStartInColliders = false;
+
+                hit = Physics2D.Raycast(transform.position, Vector2.right * transform.localScale.x, distance);
+
+                if (hit.collider != null && hit.collider.tag == "grabbable")
+                {
+                    grabbed = true;
+                    Debug.Log("werkt");
+                }
+            }
+            else if (!Physics2D.OverlapPoint(holdpoint.position, notgrabbed))
+            {
+                grabbed = false;
+
+                if (hit.collider.gameObject.GetComponent<Rigidbody2D>() != null)
+                {
+
+                    hit.collider.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(transform.localScale.x, 1) * throwforce;
+                }
+            }
+        }
+        if (grabbed)
+            hit.collider.gameObject.transform.position = holdpoint.position;
+        
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+
+        Gizmos.DrawLine(transform.position, transform.position + Vector3.right * transform.localScale.x * distance);
+    }
+}*/
 }
