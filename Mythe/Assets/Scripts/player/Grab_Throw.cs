35127d09cbf2990b6841ca5b2edec3c2d7d83460
nickvaningen <nickvaningen@hotmail.com>
2018-03-16 10:55:19 +0100

    gooien is af

--- Mythe/Assets/Scripts/player/Grab_Throw.cs
+++ Mythe/Assets/Scripts/player/Grab_Throw.cs
@@ -3,52 +3,79 @@ using System.Collections;
 
 public class Grab_Throw : MonoBehaviour
 {
+    //  var hit : RaycastHit
+    public float pushforce;
+    public float pullforce = 1;
+    private GameObject player;
+    private Walk _playerWalk;
+    private Rigidbody _rb;
+    private float _normSpeed;
+    private bool Dragging = false;
+    private GameObject target;
+    Vector3 targetposition;
+    float diffX;
+    [SerializeField]
+    private float throwforce = 5;
 
-    public bool grabbed;
-    RaycastHit hit;
-    public float distance = 2f;
-    public Transform holdpoint;
-    public float throwforce;
-    public LayerMask notgrabbed;
+
+    void Start()
+    {
+        _playerWalk = GetComponent<Walk>();
+        _rb = GetComponent<Rigidbody>();
+        _normSpeed = _playerWalk.moveSpeed;
+    }
+
     void Update()
     {
 
-        if (Input.GetKeyDown(KeyCode.LeftShift))
+        if (Dragging)
         {
-        
-            if (!grabbed)
+            print("drag");
+
+
+            //
+
+
+
+            targetposition = new Vector3(transform.position.x + diffX, target.transform.position.y, transform.position.z);
+            target.GetComponent<Rigidbody>().MovePosition(targetposition);
+            if (Input.GetKeyUp(KeyCode.L))
             {
-                /*Physics.queriesStartInColliders = false;*/
-
-                hit = Physics.Raycast(transform.position, Vector2.right * transform.localScale.x, distance);
-
-                if (hit.collider != null && hit.collider.tag == "grabbable")
+                print("stop");
+                Dragging = false;
+                if (target.tag == "throwObject")
                 {
-                    grabbed = true;
-                    Debug.Log("werkt");
+                    target.GetComponent<Rigidbody>().AddForce(new Vector3(1, 1, 0) * throwforce, ForceMode.Impulse);
                 }
-            }
-            else if (!Physics2D.OverlapPoint(holdpoint.position, notgrabbed))
-            {
-                grabbed = false;
-
-                if (hit.collider.gameObject.GetComponent<Rigidbody>() != null)
+                else
                 {
-
-                    hit.collider.gameObject.GetComponent<Rigidbody>().velocity = new Vector3(transform.localScale.x, 1,0) * throwforce;
+                    _playerWalk.moveSpeed = _normSpeed;
+                    target.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezeRotation;
                 }
+                
             }
         }
-        if (grabbed)
-            hit.collider.gameObject.transform.position = holdpoint.position;
-        
+
+
+
+
     }
 
-    void OnDrawGizmos()
+    void OnCollisionStay(Collision other)
     {
-        Gizmos.color = Color.green;
+        if (other.gameObject.tag == "MoveAble"|| other.gameObject.tag == "throwObject")
+        {
+            Debug.Log("im touching");
+            if (Input.GetKeyDown(KeyCode.L))
+            {
+                Dragging = true;
+                target = other.gameObject;
+                diffX = target.transform.position.x - transform.position.x;
+                target.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+                //_playerWalk.moveSpeed *= (pushforce / 2);
 
-        Gizmos.DrawLine(transform.position, transform.position + Vector3.right * transform.localScale.x * distance);
+            }
+        }
     }
     /*using UnityEngine;
 using System.Collections;
