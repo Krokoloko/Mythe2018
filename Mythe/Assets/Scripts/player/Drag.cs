832b0690d35b1cd39600071c967445cac15bc996
nickvaningen <nickvaningen@hotmail.com>
2018-03-16 11:04:14 +0100

    het gooien en het dowen en trekken zitten nu in 1 script

--- Mythe/Assets/Scripts/player/Drag.cs
+++ Mythe/Assets/Scripts/player/Drag.cs
@@ -11,11 +11,13 @@ public class Drag : MonoBehaviour
     private GameObject player;
     private Walk _playerWalk;
     private Rigidbody _rb;
-    private float _normSpeed; 
+    private float _normSpeed;
     private bool Dragging = false;
     private GameObject target;
     Vector3 targetposition;
     float diffX;
+    [SerializeField]
+    private float throwforce = 5;
 
 
     void Start()
@@ -25,42 +27,47 @@ public class Drag : MonoBehaviour
         _normSpeed = _playerWalk.moveSpeed;
     }
 
-void Update()
+    void Update()
     {
 
         if (Dragging)
         {
             print("drag");
 
-           
+
             //
 
-            
-            
-            targetposition = new Vector3(transform.position.x + diffX,target.transform.position.y,transform.position.z);
+
+
+            targetposition = new Vector3(transform.position.x + diffX, target.transform.position.y, transform.position.z);
             target.GetComponent<Rigidbody>().MovePosition(targetposition);
             if (Input.GetKeyUp(KeyCode.L))
             {
                 print("stop");
                 Dragging = false;
-                _playerWalk.moveSpeed = _normSpeed;
-                target.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezeRotation;
+                if (target.tag == "throwObject")
+                {
+                    target.GetComponent<Rigidbody>().AddForce(new Vector3(1, 1, 0) * throwforce, ForceMode.Impulse);
+                }
+                else
+                {
+                    _playerWalk.moveSpeed = _normSpeed;
+                    target.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezeRotation;
+                }
+
             }
         }
-        
-     
+
+
 
 
     }
 
     void OnCollisionStay(Collision other)
     {
-        if (other.gameObject.tag == "MoveAble")
+        if (other.gameObject.tag == "MoveAble" || other.gameObject.tag == "throwObject")
         {
             Debug.Log("im touching");
-
-           
-
             if (Input.GetKeyDown(KeyCode.L))
             {
                 Dragging = true;
@@ -70,7 +77,6 @@ void Update()
                 //_playerWalk.moveSpeed *= (pushforce / 2);
 
             }
-            
         }
     }
 }
