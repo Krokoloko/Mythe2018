6b57ef30d38f8c8947e8747f62db91c026799368
VolkenK <volkenkoelewijn@gmail.com>
2018-03-19 15:39:46 +0100

    button script
    also start on gate script

---
+++ Mythe/Assets/Scripts/Gate.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gate : MonoBehaviour {
+
+    public GameObject button;
+    private Button _button;
+    private Transform _origin;
+    private float _limit;
+    public string direction;
+    public float distance = 3;
+
+    private void OnButton()
+    {
+        if (_button.active)
+        {
+            switch (direction)
+            {
+                case"up":
+                    if (_limit >= transform.position.y)
+                    {
+                        transform.Translate(Vector3.up*2*Time.deltaTime);
+                    }
+                    else
+                    {
+                        print("stop");
+                    }
+                    break;
+                case"down":
+                    transform.Translate(Vector3.down);
+                    break;
+                case"left":
+                    transform.Translate(Vector3.left);
+                    break;
+                case "right":
+                    transform.Translate(Vector3.right);
+                    break;
+            }
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+        _origin = transform;
+        _limit = _origin.position.y + distance;
+        _button = button.GetComponent<Button>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        OnButton();
+	}
+}
