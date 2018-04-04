b0ee06b4e5eb4f925bf965baf71d6bb2e07b6bb2
Daan Ruting <daan.j.ruting@gmail.com>
2018-04-04 14:06:08 +0200

    working on climb mechanic

---
+++ Mythe/Assets/Scripts/Objects/Ladder.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ladder : MonoBehaviour {
+
+    private GameObject _player;
+    public bool active;
+
+	void Start () {
+        _player = GameObject.FindGameObjectWithTag("Player");
+        if (!GetComponent<BoxCollider>().isTrigger)
+        {
+            GetComponent<BoxCollider>().isTrigger = true;
+        }
+	}
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("player is in range");
+            active = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            active = false;
+        }
+    }
+}
--- Mythe/Assets/Scripts/player/Climb.cs
+++ Mythe/Assets/Scripts/player/Climb.cs
@@ -4,13 +4,153 @@ using UnityEngine;
 
 public class Climb : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+
+    public enum ClimbState {none, climbingNeutral, climbingUp, climbingDown};
+    public ClimbState state = ClimbState.none;
+    private GameObject _player;
+    private Walk _walk;
+    private Jump _jump;
+    private Drag _drag;
+    private Crouch _crouch;
+
+    private bool _climbing;
+
+    private GameObject[] _ladders;
+
+	void Start ()
+    {
+
+        _player = GameObject.FindGameObjectWithTag("Player");
+        _walk = _player.GetComponent<Walk>();
+        _jump = _player.GetComponent<Jump>();
+        _drag = _player.GetComponent<Drag>();
+        _crouch = _player.GetComponent<Crouch>();
+        _ladders = GameObject.FindGameObjectsWithTag("ladder");
+
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    void Update()
+    {
+        RoutineSwitch();
+    }
+
+    private void RoutineSwitch()
+    {
+        switch (state)
+        {
+            case ClimbState.none:
+
+                if (InRange() && Input.GetKey(KeyCode.W))
+                {
+                    state = ClimbState.climbingNeutral;
+                }
+                break;
+
+            case ClimbState.climbingDown:
+
+                if (!InRange())
+                {
+                    state = ClimbState.none;
+                }
+                if (Input.GetKey(KeyCode.S))
+                {
+                    if (Input.GetKey(KeyCode.W))
+                    {
+                        state = ClimbState.climbingNeutral;
+                    }
+                }
+                else
+                {
+                    state = ClimbState.none;
+                }
+                break;
+
+            case ClimbState.climbingUp:
+
+                if (!InRange())
+                {
+                    state = ClimbState.none;
+                }
+                if (Input.GetKey(KeyCode.W))
+                {
+                    if (Input.GetKey(KeyCode.S))
+                    {
+                        state = ClimbState.climbingNeutral;
+                    }
+                }
+                else
+                {
+                    state = ClimbState.none;
+                }
+                break;
+
+            case ClimbState.climbingNeutral:
+
+                if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.W))
+                {
+                    break;
+                }
+                if (Input.GetKey(KeyCode.W))
+                {
+                    state = ClimbState.climbingUp;
+                }
+                if (Input.GetKey(KeyCode.S))
+                {
+                    state = ClimbState.climbingDown;
+                }               
+                break;
+        }
+    }
+
+    private void ClimbRoutine()
+    {
+        switch (state)
+        {
+            case ClimbState.none:
+                _climbing = false;
+                break;
+
+            case ClimbState.climbingDown:
+                _climbing = true;
+                break;
+
+            case ClimbState.climbingUp:
+                _climbing = true;
+                break;
+
+            case ClimbState.climbingNeutral:
+                _climbing = true;
+                break;
+        }
+    }
+
+    private void SwitchScripts()
+    {
+        if (_climbing)
+        {
+            _walk.enabled = !_climbing;
+            _jump.enabled = !_climbing;
+            _drag.enabled = !_climbing;
+            _crouch.enabled = !_climbing;
+        }
+        else
+        {
+            _walk.enabled = _climbing;
+            _jump.enabled = _climbing;
+            _drag.enabled = _climbing;
+            _crouch.enabled = _climbing;
+        }
+    }
+
+    private bool InRange()
+    {
+        for (int i = 0; i < _ladders.Length; i++)
+        {
+            if (_ladders[i].GetComponent<Ladder>().active)
+            {
+                return true;
+            }
+        }
+        return false;
+    } 
 }
